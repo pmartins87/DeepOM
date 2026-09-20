42a646f3f07e7c5016712a427df48f532922524f
SirSpin <pmartinsjr87@gmail.com>
2026-09-20 13:51:11 -0300

    Expose DeepOM evaluator API

---
+++ deepom/__init__.py
@@ -0,0 +1,19 @@
+from .evaluator import (
+    CATEGORY_NAME,
+    HandRank,
+    best_omaha_five,
+    evaluate_five,
+    evaluate_omaha,
+    is_royal_flush,
+    qualifies_aof_omaha_jackpot,
+)
+
+__all__ = [
+    "CATEGORY_NAME",
+    "HandRank",
+    "best_omaha_five",
+    "evaluate_five",
+    "evaluate_omaha",
+    "is_royal_flush",
+    "qualifies_aof_omaha_jackpot",
+]
