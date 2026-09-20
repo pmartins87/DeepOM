c0ce091f4135b1405cafa8f94bcb3b81c4f24cfe
SirSpin <pmartinsjr87@gmail.com>
2026-09-20 13:57:46 -0300

    Expose OM2 equity API

--- deepom/__init__.py
+++ deepom/__init__.py
@@ -7,13 +7,31 @@ from .evaluator import (
     is_royal_flush,
     qualifies_aof_omaha_jackpot,
 )
+from .equity import (
+    DECK,
+    EquityResult,
+    exact_equity_known_hands,
+    monte_carlo_equity_known_hands,
+    monte_carlo_equity_vs_random,
+    qualifying_royal_suits,
+    royal_flush_flop_probability,
+    royal_flush_full_board_probability,
+)
 
 __all__ = [
     "CATEGORY_NAME",
+    "DECK",
+    "EquityResult",
     "HandRank",
     "best_omaha_five",
     "evaluate_five",
     "evaluate_omaha",
+    "exact_equity_known_hands",
     "is_royal_flush",
+    "monte_carlo_equity_known_hands",
+    "monte_carlo_equity_vs_random",
     "qualifies_aof_omaha_jackpot",
+    "qualifying_royal_suits",
+    "royal_flush_flop_probability",
+    "royal_flush_full_board_probability",
 ]
