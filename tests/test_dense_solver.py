b4afccd736eb399f11ecc678e2fae7aedde8ed5e
SirSpin <pmartinsjr87@gmail.com>
2026-09-20 14:55:45 -0300

    Add dense trainer checkpoint and regression tests

---
+++ tests/test_dense_solver.py
@@ -0,0 +1,88 @@
+import tempfile
+import unittest
+
+import numpy as np
+
+from deepom.aof_kernel import ALLIN, apply_action, initial_state
+from deepom.dense_solver import DenseExternalSamplingCFR, PLO4ClassIndex
+from deepom.economics import GG_AOF_OMAHA_020_040_JP750K_F75K_RB35_V0
+from deepom.solver_proto import SampledDeal
+
+
+class DenseSolverTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.index = PLO4ClassIndex.build()
+
+    def test_full_class_index(self):
+        self.assertEqual(len(self.index.keys), 16_432)
+        self.assertEqual(len(self.index.sha256), 64)
+
+    def test_dense_core_size_is_small(self):
+        s = DenseExternalSamplingCFR(
+            mode="4w",
+            class_index=self.index,
+            seed=1,
+        )
+        self.assertEqual(s.regrets.shape, (14, 16_432, 2))
+        self.assertLess(s.bytes_core, 9 * 1024 * 1024)
+
+    def test_known_losing_call_pushes_bb_to_fold(self):
+        deal = SampledDeal(
+            hole_cards=(
+                tuple("As Ks 2c 3d".split()),
+                tuple("Ah 9h 4c 5d".split()),
+            ),
+            board_cards=tuple("Qs Js Ts 7c 8d".split()),
+        )
+        s = DenseExternalSamplingCFR(
+            mode="2w",
+            class_index=self.index,
+            seed=1,
+        )
+        root = initial_state("2w")
+        sb_sc, sb_hi = s.info_indices(root, 0, deal)
+        s.regrets[sb_sc, sb_hi] = np.array([0.0, 1.0])
+
+        bb_state = apply_action(root, ALLIN)
+        bb_sc, bb_hi = s.info_indices(bb_state, 1, deal)
+        s.update_target_on_deal(deal, target_role=1)
+
+        fold_p, allin_p = s.current_strategy(bb_sc, bb_hi)
+        self.assertEqual((fold_p, allin_p), (1.0, 0.0))
+
+    def test_checkpoint_resume_matches_continuous_run(self):
+        a = DenseExternalSamplingCFR(
+            mode="2w",
+            class_index=self.index,
+            seed=123,
+            economic_preset=GG_AOF_OMAHA_020_040_JP750K_F75K_RB35_V0,
+        )
+        b = DenseExternalSamplingCFR(
+            mode="2w",
+            class_index=self.index,
+            seed=123,
+            economic_preset=GG_AOF_OMAHA_020_040_JP750K_F75K_RB35_V0,
+        )
+
+        a.run(additional_iterations=2, deals_per_iteration=2)
+
+        b.run(additional_iterations=1, deals_per_iteration=2)
+        with tempfile.TemporaryDirectory() as td:
+            b.save_checkpoint(td)
+            b2 = DenseExternalSamplingCFR.load_checkpoint(
+                td,
+                class_index=self.index,
+                economic_preset=GG_AOF_OMAHA_020_040_JP750K_F75K_RB35_V0,
+            )
+            b2.run(additional_iterations=1, deals_per_iteration=2)
+
+        np.testing.assert_array_equal(a.regrets, b2.regrets)
+        np.testing.assert_array_equal(a.strategy_sum, b2.strategy_sum)
+        np.testing.assert_array_equal(a.visits, b2.visits)
+        self.assertEqual(a.iteration_completed, b2.iteration_completed)
+        self.assertEqual(a.manifest()["arrays_sha256"], b2.manifest()["arrays_sha256"])
+
+
+if __name__ == "__main__":
+    unittest.main()
